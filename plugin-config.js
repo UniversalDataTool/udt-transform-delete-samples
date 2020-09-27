ee00a1535a82d46b2fe6c9ffcac0b968f1dcff76
seveibar <seveibar@gmail.com>
2020-09-27 16:07:48 -0400

    made es6 compatible, published version 1.0.3

--- plugin-config.js
+++ plugin-config.js
@@ -1,13 +1,17 @@
 // This is the first file loaded and defines what things your plugin can do!
 
-module.exports = {
-  transformPlugins: [
-    {
-      name: "Delete All Samples",
-      renderDialog: require("./src/render-dialog"),
-    },
-  ],
-  importPlugins: [],
-  interfacePlugins: [],
-  authenticationPlugins: [],
+import renderDialog from "./src/render-dialog.js"
+
+export default () => {
+  return {
+    transformPlugins: [
+      {
+        name: "Delete All Samples",
+        renderDialog,
+      },
+    ],
+    importPlugins: [],
+    interfacePlugins: [],
+    authenticationPlugins: [],
+  }
 }
